5c9127fc4232feb8edd40c84b992dfaaf9680213
Sylwester <sylwesterbartnicki@gmail.com>
2019-10-05 14:10:40 +0200

    Sala

--- src/views/Reservation/Reservation.js
+++ src/views/Reservation/Reservation.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReservationForm from '../../components/ReservationForm/ReservationForm';
 import AppContext from '../../context';
+import Theather from '../../components/Theater/Theater';
+
 
 const Reservation = () => {
   return (
+    <>
+    <Theather />
     <AppContext.Consumer>
       {context => (<h1><ReservationForm reservation={{ ...context }} /></h1>)}
     </AppContext.Consumer>
+    </>
   );
 };
 
